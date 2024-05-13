13fde2dfc5334a9cbf0d9f4badc18f6abd714f7e
Roman Krasiuk <rokrassyuk@gmail.com>
2024-05-13 15:48:05 +0300

    accept ref

--- src/proof/verify.rs
+++ src/proof/verify.rs
@@ -11,14 +11,14 @@ use alloy_rlp::Decodable;
 use nybbles::Nibbles;
 
 /// Verify the proof for given key value pair against the provided state root.
-pub fn verify_proof<I>(
+pub fn verify_proof<'a, I>(
     proof: I,
     root: B256,
     key: B256,
     value: Vec<u8>,
 ) -> Result<(), ProofVerificationError>
 where
-    I: IntoIterator<Item = Bytes>,
+    I: IntoIterator<Item = &'a Bytes>,
     I::IntoIter: DoubleEndedIterator,
 {
     let mut proof = proof.into_iter().rev().peekable();
@@ -113,12 +113,12 @@ mod tests {
         let mut hash_builder = HashBuilder::default().with_proof_retainer(ProofRetainer::default());
         let root = hash_builder.root();
         let proof = hash_builder.take_proofs();
-        assert_eq!(verify_proof(proof.into_values(), root, key, vec![]), Ok(()));
+        assert_eq!(verify_proof(proof.values(), root, key, vec![]), Ok(()));
 
         let mut dummy_proof = vec![];
         BranchNode::default().encode(&mut dummy_proof);
         assert_eq!(
-            verify_proof([Bytes::from(dummy_proof)], root, key, vec![]),
+            verify_proof([&Bytes::from(dummy_proof)], root, key, vec![]),
             Err(ProofVerificationError::missing_branch_child(Nibbles::unpack(key)))
         );
     }
@@ -134,7 +134,7 @@ mod tests {
         assert_eq!(root, triehash_trie_root([(target, target)]));
 
         let proof = hash_builder.take_proofs();
-        assert_eq!(verify_proof(proof.into_values(), root, target, target.to_vec()), Ok(()));
+        assert_eq!(verify_proof(proof.values(), root, target, target.to_vec()), Ok(()));
     }
 
     #[test]
@@ -155,7 +155,7 @@ mod tests {
         );
 
         let proof = hash_builder.take_proofs();
-        assert_eq!(verify_proof(proof.into_values(), root, target, target.to_vec()), Ok(()));
+        assert_eq!(verify_proof(proof.values(), root, target, target.to_vec()), Ok(()));
     }
 
     #[test]
@@ -178,16 +178,12 @@ mod tests {
 
         let proof = hash_builder.take_proofs();
 
-        let proof1 = proof
-            .clone()
-            .into_iter()
-            .filter_map(|(k, v)| Nibbles::unpack(target1).starts_with(&k).then_some(v));
+        let proof1 =
+            proof.iter().filter_map(|(k, v)| Nibbles::unpack(target1).starts_with(&k).then_some(v));
         assert_eq!(verify_proof(proof1, root, target1, target1.to_vec()), Ok(()));
 
-        let proof2 = proof
-            .clone()
-            .into_iter()
-            .filter_map(|(k, v)| Nibbles::unpack(target2).starts_with(&k).then_some(v));
+        let proof2 =
+            proof.iter().filter_map(|(k, v)| Nibbles::unpack(target2).starts_with(&k).then_some(v));
         assert_eq!(verify_proof(proof2, root, target2, target2.to_vec()), Ok(()));
     }
 
@@ -214,7 +210,7 @@ mod tests {
 
             let proofs = hash_builder.take_proofs();
             for (key, value) in hashed {
-                let proof = proofs.iter().filter_map(|(k, v)| Nibbles::unpack(key).starts_with(k).then_some(v.clone()));
+                let proof = proofs.iter().filter_map(|(k, v)| Nibbles::unpack(key).starts_with(k).then_some(v));
                 assert_eq!(verify_proof(proof, root, key, value), Ok(()));
             }
         });
