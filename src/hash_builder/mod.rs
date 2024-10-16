cf984afb87802665c8e750f74dccc4b7c167c5f7
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-10-16 21:55:32 +0100

    chore(hash-builder): use `RlpNode::as_hash` (#59)

--- src/hash_builder/mod.rs
+++ src/hash_builder/mod.rs
@@ -170,8 +170,8 @@ impl HashBuilder {
 
     fn current_root(&self) -> B256 {
         if let Some(node_ref) = self.stack.last() {
-            if node_ref.len() == B256::len_bytes() + 1 {
-                B256::from_slice(&node_ref[1..])
+            if let Some(hash) = node_ref.as_hash() {
+                hash
             } else {
                 keccak256(node_ref)
             }
