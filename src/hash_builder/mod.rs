5c240ec4a5a4a7cf6892354b27d5deb36d8a42e4
moricho <ikeda.morito@gmail.com>
2024-12-05 00:24:57 +0900

    Disable `option-if-let-els` lint

--- src/hash_builder/mod.rs
+++ src/hash_builder/mod.rs
@@ -169,9 +169,15 @@ impl HashBuilder {
     }
 
     fn current_root(&self) -> B256 {
-        self.stack.last().map_or(EMPTY_ROOT_HASH, |node_ref| {
-            node_ref.as_hash().map_or_else(|| keccak256(node_ref), |hash| hash)
-        })
+        if let Some(node_ref) = self.stack.last() {
+            if let Some(hash) = node_ref.as_hash() {
+                hash
+            } else {
+                keccak256(node_ref)
+            }
+        } else {
+            EMPTY_ROOT_HASH
+        }
     }
 
     /// Given a new element, it appends it to the stack and proceeds to loop through the stack state
