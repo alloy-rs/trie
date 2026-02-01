edc42d20ce932f16858b7fe558dc43b070a58a38
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-02-01 20:52:23 +0100

    chore: use thiserror for Error implementations (#127)

--- src/proof/error.rs
+++ src/proof/error.rs
@@ -1,11 +1,11 @@
 use alloy_primitives::{B256, Bytes};
-use core::fmt;
 use nybbles::Nibbles;
 
 /// Error during proof verification.
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, thiserror::Error)]
 pub enum ProofVerificationError {
     /// State root does not match the expected.
+    #[error("root mismatch. got: {got}. expected: {expected}")]
     RootMismatch {
         /// Computed state root.
         got: B256,
@@ -13,6 +13,7 @@ pub enum ProofVerificationError {
         expected: B256,
     },
     /// The node value does not match at specified path.
+    #[error("value mismatch at path {path:?}. got: {got:?}. expected: {expected:?}")]
     ValueMismatch {
         /// Path at which error occurred.
         path: Nibbles,
@@ -22,45 +23,9 @@ pub enum ProofVerificationError {
         expected: Option<Bytes>,
     },
     /// Encountered unexpected empty root node.
+    #[error("unexpected empty root node")]
     UnexpectedEmptyRoot,
     /// Error during RLP decoding of trie node.
-    Rlp(alloy_rlp::Error),
-}
-
-/// Enable Error trait implementation when core is stabilized.
-/// <https://github.com/rust-lang/rust/issues/103765>
-#[cfg(feature = "std")]
-impl std::error::Error for ProofVerificationError {
-    fn source(&self) -> ::core::option::Option<&(dyn std::error::Error + 'static)> {
-        #[allow(deprecated)]
-        match self {
-            Self::Rlp { 0: transparent } => {
-                std::error::Error::source(transparent as &dyn std::error::Error)
-            }
-            _ => None,
-        }
-    }
-}
-
-impl fmt::Display for ProofVerificationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::RootMismatch { got, expected } => {
-                write!(f, "root mismatch. got: {got}. expected: {expected}")
-            }
-            Self::ValueMismatch { path, got, expected } => {
-                write!(f, "value mismatch at path {path:?}. got: {got:?}. expected: {expected:?}")
-            }
-            Self::UnexpectedEmptyRoot => {
-                write!(f, "unexpected empty root node")
-            }
-            Self::Rlp(error) => fmt::Display::fmt(error, f),
-        }
-    }
-}
-
-impl From<alloy_rlp::Error> for ProofVerificationError {
-    fn from(source: alloy_rlp::Error) -> Self {
-        Self::Rlp(source)
-    }
+    #[error(transparent)]
+    Rlp(#[from] alloy_rlp::Error),
 }
